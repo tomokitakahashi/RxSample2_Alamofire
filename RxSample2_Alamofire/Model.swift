e92997169fcadee99e5671eb7041db7f510b9ada
takahashi tomoki <tomoki@MacBook-Pro-2.local>
2016-09-12 00:17:06 +0900

    グループの追加

---
+++ RxSample2_Alamofire/Model.swift
@@ -0,0 +1,9 @@
+//
+//  Model.swift
+//  RxSample2_Alamofire
+//
+//  Created by takahashi tomoki on 2016/09/12.
+//  Copyright © 2016年 takahashi tomoki. All rights reserved.
+//
+
+import Foundation
